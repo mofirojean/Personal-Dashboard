b6bab1bb58e280c4a9d457e51d1f717166a79718
mofirojean <mofirojean@gmail.com>
2022-12-28 19:23:19 +0100

    feat: added the service class for add note

---
+++ src/app/service/note.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Note } from './note.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteService {
+
+  notes!: Note[]
+
+  constructor() { }
+
+  getNotes() {
+    return this.notes
+  }
+
+  getNote(id: string){
+    return this.getNotes().find(n => n.id === id) // returns the note object
+  }
+
+  addNote(note: Note) {
+    this.notes.push(note)
+  }
+
+  updateNote(id: string, updatedFields: Partial<Note>) {
+    const note = this.getNote(id)
+    Object.assign(note, updatedFields)
+  }
+
+  deleteNote(id: string) {
+    const noteIndex = this.notes?.findIndex(n => n.id === id)
+    if (noteIndex === -1) return 
+    this.notes.splice(noteIndex, 1)
+  }
+}
