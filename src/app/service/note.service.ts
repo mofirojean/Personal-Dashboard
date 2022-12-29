f25b4002767d93db5bb39821673c26bfb4144cc0
mofirojean <mofirojean@gmail.com>
2022-12-29 11:14:09 +0100

    fix: added if condition to updateNote() method

--- src/app/service/note.service.ts
+++ src/app/service/note.service.ts
@@ -6,7 +6,10 @@ import { Note } from './note.model';
 })
 export class NoteService {
 
-  notes!: Note[]
+  notes: Note[] = [
+    new Note("Test title", "Test content"),
+    new Note("Test 2", "Test content 2")
+  ]
 
   constructor() { }
 
@@ -23,12 +26,15 @@ export class NoteService {
   }
 
   updateNote(id: string, updatedFields: Partial<Note>) {
-    const note:Note = this.getNote(id)
-    Object.assign(note, updatedFields)
+    const note = this.getNote(id)
+    // test if the note is object before updating the fields
+    if (typeof note === 'object') {
+      Object.assign(note, updatedFields)
+    }
   }
 
   deleteNote(id: string) {
-    const noteIndex = this.notes?.findIndex(n => n.id === id)
+    const noteIndex = this.notes.findIndex(n => n.id === id)
     if (noteIndex === -1) return 
     this.notes.splice(noteIndex, 1)
   }
