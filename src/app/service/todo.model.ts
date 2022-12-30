2bd9dead2c2c8688b3a6d3a5ae656671b86f4b32
mofirojean <mofirojean@gmail.com>
2022-12-30 18:47:41 +0100

    feat:
    created a model for the todo to show the structure of the data

---
+++ src/app/service/todo.model.ts
@@ -0,0 +1,10 @@
+import { v4 as uuidav4 } from "uuid"
+
+export class Todo {
+    id: string
+    completed?: boolean
+
+    constructor(public text: string, ) {
+        this.id = uuidav4()
+    }
+}
