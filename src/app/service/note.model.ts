f821667a4d7f420556cab2e7621b332aa0fc9ad1
mofirojean <mofirojean@gmail.com>
2022-12-30 12:01:04 +0100

    docs: added more information to the model

--- src/app/service/note.model.ts
+++ src/app/service/note.model.ts
@@ -1,8 +1,13 @@
 import {v4 as uuidv4} from "uuid"
 
 export class Note {
-    id?: string
+    id: string
 
+    /**
+     * 
+     * @param title Text that signifies the title of the note
+     * @param content Text that signifies the body of the note
+     */
     constructor(public title: string, public content: string) {
         this.id = uuidv4()
     }
