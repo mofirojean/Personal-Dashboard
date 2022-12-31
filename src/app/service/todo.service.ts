5abfbf5fb3ada0166457d6e034499f852f5784cb
mofirojean <mofirojean@gmail.com>
2022-12-31 17:40:41 +0100

    feat: created a service class to host todos method

---
+++ src/app/service/todo.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  todos: Todo[] = [
+    new Todo("This is a test"),
+    new Todo("This is a test")
+  ]
+
+  constructor() {
+   }
+
+  getTodos() {
+    return this.todos
+  }
+
+  getTodo(id: string) {
+    return this.todos.find(t => t.id === id) // gets the unique todo
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo)
+  }
+
+  updateTodo(id: string, updatedTodoFields: Partial<Todo>) {
+    const todo = this.getTodo(id)
+    if (typeof todo == 'object') {
+      Object.assign(todo, updatedTodoFields)
+    }
+  }
+
+  deleteTodo(id: string) {
+    const index = this.todos.findIndex(t => t.id === id)
+    if (index == -1) return
+    this.todos.splice(index, 1)
+  }
+}
