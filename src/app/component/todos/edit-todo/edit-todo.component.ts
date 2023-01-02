f41c7116da813127b4adfab37d4c975476dac096
mofirojean <mofirojean@gmail.com>
2023-01-02 14:51:58 +0100

    feat: Implemented the edit-todo view

--- src/app/component/todos/edit-todo/edit-todo.component.ts
+++ src/app/component/todos/edit-todo/edit-todo.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Todo } from 'src/app/model/todo.model';
+import { TodoService } from 'src/app/service/todo.service';
 
 @Component({
   selector: 'app-edit-todo',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditTodoComponent implements OnInit {
 
-  constructor() { }
+  todo?: Todo
+
+  constructor(private todoService: TodoService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      const todoId = paramMap.get('id') // this will grap it from the router link [todos/:id]
+      if (typeof todoId == "string") {
+        this.todo = this.todoService.getTodo(todoId)
+      }
+    })
   }
 
+  onFormSubmit(form: NgForm) {
+    if (form.invalid) return
+    if (typeof this.todo?.id == 'string') {
+      this.todoService.updateTodo(this.todo?.id, form.value)
+    }
+    this.router.navigateByUrl('/todos')
+  }
 }
