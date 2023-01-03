ff5906cc8698fcb138593792bbd40a149230586d
mofirojean <mofirojean@gmail.com>
2023-01-03 13:52:01 +0100

    feat: created the add book marks UI

--- src/app/component/bookmarks/add-bookmark/add-bookmark.component.ts
+++ src/app/component/bookmarks/add-bookmark/add-bookmark.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Bookmark } from 'src/app/model/bookmark.model';
+import { BookmarkService } from 'src/app/service/bookmark.service';
 
 @Component({
   selector: 'app-add-bookmark',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBookmarkComponent implements OnInit {
 
-  constructor() { }
+  constructor(private bookmarkSevice: BookmarkService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onFormSubmit(form: NgForm) {
+    const {name, url} = form.value
+    const bookmark = new Bookmark(name, url)
+    this.bookmarkSevice.addBookmark(bookmark)
+    this.router.navigateByUrl('/bookmarks')
+  }
 }
