41704949a59c85a48875dd8be92c03607e2b158f
mofirojean <mofirojean@gmail.com>
2023-01-03 13:49:45 +0100

    feat: added the path to the add bookmarks view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,11 @@ import { AddNoteComponent } from './component/notes/add-note/add-note.component'
 import { EditNoteComponent } from './component/notes/edit-note/edit-note.component';
 import { AddTodoComponent } from './component/todos/add-todo/add-todo.component';
 import { EditTodoComponent } from './component/todos/edit-todo/edit-todo.component';
+import { AddBookmarkComponent } from './component/bookmarks/add-bookmark/add-bookmark.component';
 
 const routes: Routes = [
   {path: "bookmarks", component: BookmarksComponent, data: {tab: 1}},
+  {path: "bookmarks/add", component: AddBookmarkComponent},
   {path: "todos", component: TodosComponent, data: {tab: 2}},
   {path: "todos/add", component: AddTodoComponent},
   {path: "todos/:id", component: EditTodoComponent},
