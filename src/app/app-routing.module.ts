adba52c878a91414318532548a8c1c8fa9200a5d
mofirojean <mofirojean@gmail.com>
2023-01-04 10:35:39 +0100

    refactor: changed theb path of edit-bookmarks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,8 +14,13 @@ import { EditBookmarkComponent } from './component/bookmarks/manage-bookmarks/ed
 const routes: Routes = [
   {path: "bookmarks", component: BookmarksComponent, data: {tab: 1}},
   {path: "bookmarks/add", component: AddBookmarkComponent},
-  {path: "bookmarks/manage", component: ManageBookmarksComponent},
-  {path: "bookmarks/manage/:id", component: EditBookmarkComponent},
+  {
+    path: "bookmarks/manage", 
+    component: ManageBookmarksComponent, 
+    children: [
+    {path: ":id", component: EditBookmarkComponent}
+    ]
+  },
   {path: "todos", component: TodosComponent, data: {tab: 2}},
   {path: "todos/add", component: AddTodoComponent},
   {path: "todos/:id", component: EditTodoComponent},
